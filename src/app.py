cedf40a117645987535c2a04db68c1b1f6f793b5
Hector Chocobar-Torrejon <hector@chocobar.net>
2025-07-29 20:29:44 -0300

    [iris] feat: front and model

--- src/app.py
+++ src/app.py
@@ -1,6 +1,33 @@
-from utils import db_connect
+import os
+from flask import Flask, request, render_template
+from pickle import load
 
 
-engine = db_connect()
+app = Flask(__name__)
 
-# your code here
+
+model = load(open('../models/iris-decision-tree-classifier-default-18.pkl', 'rb'))
+class_dict = {"0": "Iris setosa",
+              "1": "Iris versicolor",
+              "2": "Iris virginica"}
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template('index.html', prediction=None)
+    if request.method == "POST":
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        val4 = float(request.form["val4"])
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+        return render_template('index.html', prediction=pred_class)
+    return None
+
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,45 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 import pandas as pd
+# Regression Metrics
+from sklearn.metrics import mean_squared_error, root_mean_squared_error
+from sklearn.metrics import median_absolute_error, mean_absolute_percentage_error
+from sklearn.metrics import r2_score
+# Classifier Metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
-load_dotenv()  # load the .env file variables
+def get_regression_metrics(y_predict_test, y_test, y_predict_train, y_train):
+    metrics_train = (r2_score(y_train, y_predict_train),
+                     median_absolute_error(y_train, y_predict_train),
+                     mean_absolute_percentage_error(y_train, y_predict_train) * 100,
+                     mean_squared_error(y_train, y_predict_train),
+                     root_mean_squared_error(y_train, y_predict_train))
+    metrics_test = (r2_score(y_test, y_predict_test),
+                    median_absolute_error(y_test, y_predict_test),
+                    mean_absolute_percentage_error(y_test, y_predict_test) * 100,
+                    mean_squared_error(y_test, y_predict_test),
+                    root_mean_squared_error(y_test, y_predict_test))
+    metrics_diff = list(map(lambda x: x[1] - x[0], zip(metrics_train, metrics_test)))
+    return pd.DataFrame(data=[metrics_train, metrics_test, metrics_diff],
+                        columns=['R2', 'MAE', 'MAPE', 'MSE', 'RMSE'],
+                        index=['Train set', 'Test set', 'Difference'])
 
 
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+def get_classifier_metrics(y_predict_test, y_test, y_predict_train, y_train, average='micro'):
+    metrics_train = (accuracy_score(y_train, y_predict_train),
+                     f1_score(y_train, y_predict_train, average=average),
+                     precision_score(y_train, y_predict_train, average=average),
+                     recall_score(y_train, y_predict_train, average=average))
+    metrics_test = (accuracy_score(y_test, y_predict_test),
+                    f1_score(y_test, y_predict_test, average=average),
+                    precision_score(y_test, y_predict_test, average=average),
+                    recall_score(y_test, y_predict_test, average=average))
+    return pd.DataFrame(data=[metrics_train, metrics_test],
+                        columns=['Accuracy', 'F1 Score', 'Precision', 'Recall'],
+                        index=['Train set', 'Test set'])
+
+
+
+
